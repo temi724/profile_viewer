637c847b04531651f56aa50e3be01647260b8885
Temi724 <tayodaniel0@gmail.com>
2023-02-08 16:21:24 +0000

    ji

--- middlewears/error.js
+++ middlewears/error.js
@@ -1,9 +1,9 @@
 // const winston = require('winston')
 
-module.exports = function (err, req, res, next) {
+module.exports = function(err, req, res, next) {
     // winston.addColors({ error: "red" })
     console.log(err.message, err)
 
     //Log
-    res.status(500).send('Something went wrong')
+    res.status(500).send('Something went wrong', err, err.message)
 }
