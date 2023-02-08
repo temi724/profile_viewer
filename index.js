45b7491d66ba6353dab8268c40521a046cc1d3c2
temi724 <tayodaniel0@gmail.com>
2023-02-08 14:21:00 +0100

    fix

--- index.js
+++ index.js
@@ -7,11 +7,10 @@ const app = express()
 require('dotenv').config()
 require('./startup/routes')(app)
 
-app.use(express.json({ limit: '50mb' }))
+app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(helmet())
-
 
 
 
